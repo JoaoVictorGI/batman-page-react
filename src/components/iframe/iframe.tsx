2bcd68ec98c6a4a0b2a7881b73a9baee5735addf
JoaoVictorGI <joaovictor.gizidoro@pm.me>
2024-07-01 18:17:15 -0300

    perf: preconnect iframe

--- src/components/iframe/iframe.tsx
+++ src/components/iframe/iframe.tsx
@@ -2,11 +2,14 @@ export function YoutubeEmbed({ embedId }: { embedId: string }) {
   return (
     <div>
       <iframe
+        rel="preconnect"
         width={"800"}
         height={"600"}
         src={`https://www.youtube.com/embed/${embedId}`}
         frameBorder="0"
-        allow={"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"}
+        allow={
+          "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        }
         allowFullScreen
         title={"Embedded youtube"}
       />
