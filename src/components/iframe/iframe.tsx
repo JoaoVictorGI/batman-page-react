b2e873cbc4dd07a0661ad2d89946589132067bb4
JoaoVictorGI <joaovictor.gizidoro@pm.me>
2024-07-05 15:17:40 -0300

    feat: add responsivity to actor-cards and trailer

--- src/components/iframe/iframe.tsx
+++ src/components/iframe/iframe.tsx
@@ -2,11 +2,11 @@ export function YoutubeEmbed({ embedId }: { embedId: string }) {
   return (
     <div>
       <iframe
+        style={{ border: 0 }}
         rel="preconnect"
         width={"800"}
         height={"600"}
         src={`https://www.youtube.com/embed/${embedId}`}
-        frameBorder="0"
         allow={
           "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         }
--- src/components/trailer/trailer.tsx
+++ src/components/trailer/trailer.tsx
@@ -4,16 +4,9 @@ import { YoutubeEmbed } from "../iframe/iframe.tsx";
 export function Trailer() {
   return (
     <div id="w-full h-96 flex justify-center">
-      <div className="w-11/12 flex items-center justify-between">
-        {/*<video src="https://youtu.be/mqqft2x_Aa4?si=14mywjc_N2KLCeBR" className="w-1/2 mt-6">
-          <source
-            src="https://youtu.be/mqqft2x_Aa4?si=14mywjc_N2KLCeBR"
-            type="video/webm"
-          />
-          Seu navegador não possuí suporte para vídeos
-        </video>*/}
+      <div className="w-11/12 flex flex-col md:flex-row items-center justify-between">
         <YoutubeEmbed embedId="mqqft2x_Aa4" />
-        <div className="w-1/2 flex flex-col justify-between ml-7">
+        <div className="w-full md:w-1/2 flex items-center md:items-start flex-col justify-between ml-7">
           <p className="text-white text-lg w-4/6">
             Após dois anos espreitando as ruas como Batman, Bruce Wayne se
             encontra nas profundezas mais sombrias de Gotham City. Com poucos
