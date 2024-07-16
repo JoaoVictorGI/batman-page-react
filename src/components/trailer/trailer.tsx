efcf4419c10fa8d1bc39b2b26aa613e82652b160
JoaoVictorGI <joaovictor.gizidoro@pm.me>
2024-07-16 15:39:50 -0300

    revert: use default youtube embed

--- src/components/trailer/trailer.tsx
+++ src/components/trailer/trailer.tsx
@@ -1,11 +1,19 @@
-import { YouTubeEmbed } from "@next/third-parties/google";
 import { Button } from "../button/button.tsx";
 
 export function Trailer() {
   return (
     <div id="w-full h-96 flex justify-center">
       <div className="w-11/12 max-w-full flex flex-col md:flex-row items-center justify-between">
-        <YouTubeEmbed videoid="mqqft2x_Aa4" width={800} height={600} />
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube-nocookie.com/embed/mqqft2x_Aa4?si=h3kK7wPha0G4EnMR"
+          title="YouTube video player"
+          style={{ border: 0 }}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
         <div className="w-full md:w-1/2 flex items-center md:items-start flex-col justify-between ml-7">
           <p className="text-white text-lg w-4/6">
             Após dois anos espreitando as ruas como Batman, Bruce Wayne se
