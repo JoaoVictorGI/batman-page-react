197aea08cf83c91e656d3153768f8bcf30c2f1ee
JoaoVictorGI <joaovictor.gizidoro@pm.me>
2024-07-16 11:38:30 -0300

    fix: use Next YoutubeEmbed
    pnpm run dev is not working, use pnpm run build and pnpm run preview to
    view the site

--- src/components/iframe/youtube-embed.tsx
+++
@@ -1,9 +0,0 @@
-import "@justinribeiro/lite-youtube";
-
-export function YoutubeEmbed() {
-  return (
-    <div className="w-[800px] h-[600px]">
-      <lite-youtube nocookie videoid="mqqft2x_Aa4"></lite-youtube>
-    </div>
-  );
-}
--- src/components/trailer/trailer.tsx
+++ src/components/trailer/trailer.tsx
@@ -1,11 +1,11 @@
+import { YouTubeEmbed } from "@next/third-parties/google";
 import { Button } from "../button/button.tsx";
-import { YoutubeEmbed } from "../iframe/youtube-embed.tsx";
 
 export function Trailer() {
   return (
     <div id="w-full h-96 flex justify-center">
       <div className="w-11/12 max-w-full flex flex-col md:flex-row items-center justify-between">
-        <YoutubeEmbed />
+        <YouTubeEmbed videoid="mqqft2x_Aa4" width={800} height={600} />
         <div className="w-full md:w-1/2 flex items-center md:items-start flex-col justify-between ml-7">
           <p className="text-white text-lg w-4/6">
             Após dois anos espreitando as ruas como Batman, Bruce Wayne se
