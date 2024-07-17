ee70258fd26168abcd0bc7fa73f54ea2965b54c2
JoaoVictorGI <joaovictor.gizidoro@pm.me>
2024-07-17 16:58:40 -0300

    docs: add a todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Route, Routes } from "react-router-dom"
-import { Footer } from "./components/footer/footer.tsx"
-import { Header } from "./components/header/header.tsx"
-import { Contacts } from "./pages/Contacts.tsx"
-import { Home } from "./pages/Home.tsx"
-import { Photos } from "./pages/Photos.tsx"
+import { Route, Routes } from "react-router-dom";
+import { Footer } from "./components/footer/footer.tsx";
+import { Header } from "./components/header/header.tsx";
+import { Contacts } from "./pages/Contacts.tsx";
+import { Home } from "./pages/Home.tsx";
+import { Photos } from "./pages/Photos.tsx";
 
 export function App() {
   return (
@@ -13,8 +13,9 @@ export function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/contacts/" element={<Contacts />}></Route>
         <Route path="/photos/" element={<Photos />}></Route>
+        {/*TODO: Implement comments Route*/}
       </Routes>
       <Footer />
     </>
-  )
+  );
 }
